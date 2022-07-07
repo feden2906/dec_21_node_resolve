2e0f09abfdaa64debed672f552a7d0f771987152
feden2906 <feden2906@gmail.com>
2022-07-07 20:42:49 +0300

    little update

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(expressFileUpload());
+app.use('/ping', (req, res) => res.json('pong'));
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
 
